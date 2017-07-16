4ece53c048f8db68abfd65a08c92059591e5646f
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-07-16 18:19:11 +0530

    Update Q17_Yourclass.py

--- Chapter 3/Q17_Yourclass.py
+++ Chapter 3/Q17_Yourclass.py
@@ -4,7 +4,7 @@ class Yourclass:
     marks=10 #class variable
     name="ABC"
     
-    def __init__(Self,marks,name):
+    def __init__(self,marks,name):
         self.marks=marks #instance attribute
         self.name=name
 
